3bda21c560e81ee8283a5bbe785e6216c8d078e8
DelyanParushev <delyan.parushev@gmail.com>
2025-09-04 00:48:09 +0300

    Add Vercel config and simplify Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
+    outDir: 'dist'
   }
 })
